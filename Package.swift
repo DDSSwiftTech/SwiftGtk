fa68df0eebd6630174fd89c627dda0f75c639aca
Bryan Dela Cruz <bdshi13@gmail.com>
2017-03-22 05:27:29 +0800

    Changed url for dependency temporarily

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 #if os(OSX)
     let gtkUrl = "https://github.com/TomasLinhart/CGtk-OSX"
 #elseif os(Linux)
-    let gtkUrl = "https://github.com/TomasLinhart/CGtk-Linux"
+    let gtkUrl = "https://github.com/bmdelacruz/CGtk-Linux"
 #else
     fatalError("Unsupported platform.")
 #endif
@@ -11,6 +11,6 @@ import PackageDescription
 let package = Package(
   name:  "SwiftGtk",
   dependencies: [
-    .Package(url: gtkUrl, majorVersion: 1, minor: 1)
+    .Package(url: gtkUrl, majorVersion: 1, minor: 2)
   ]
 )
